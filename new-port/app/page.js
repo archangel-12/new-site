2ffe897e7af040976bc5c280f623deaea30b5e85
matthew alexander <alexandermatthew25@gmail.com>
2024-02-02 09:06:34 +0000

    end

--- new-port/app/page.js
+++ new-port/app/page.js
@@ -1,40 +1,50 @@
 import Image from "next/image";
 import Link from "next/link";
-import { PiHouseLight, PiGithubLogoLight } from "react-icons/pi";
+import { PiHouseLight, PiGithubLogoLight,  PiDiscordLogoLight } from "react-icons/pi";
 
 function page() {
   return (
-    <div className=" ">
+    <div className="/">
       <div className="p-5 fixed bottom-28 flex flex-col items-center justify-center mx-auto   w-full ">
         <div className="bg-neutral-600 rounded-full ">
           <Image
             height={1000}
             width={1000}
-            className="object-cover w-fit"
-            src="/emo.png"
-            alt=""
+            className="object-cover w-40 h-40 rounded-full"
+            src="https://i.pinimg.com/originals/65/eb/8a/65eb8a6b4a8966bafd26d1dee21ed97f.gif"
+            alt="yes"
           />
         </div>
-        <h1 className="font-RubikExtraBold text-6xl text-center   my-9 btn-shine">
-          JoScript Portfolio
+        <h1 className="text-5xl text-center   my-9 btn-shine">
+          oh hi there
         </h1>
 
-        <p className="text-neutral-400 lg:max-w-lg text-center font-RubikRegular ">
-          All features are done and coded by Joscript, please use this template
-          with free mind, remember to subscribe and share our videos
+        <p className="text-neutral-400 lg:max-w-lg text-center">
+          i've been known online as archangel or the nothing. welcome to my site, i guess...
         </p>
       </div>
 
       <div className=" flex justify-center">
         <div className="fixed bottom-7 flex border border-neutral-600 rounded-lg p-2 gap-x-5 text-neutral-500">
+
           <Link href={"/dashboard"}>
             <span>
-              <PiHouseLight className="text-2xl" />
+              <PiHouseLight className="text-3xl" />
             </span>
           </Link>
+
+          <Link href={"https://discord.gg/DHWfHRWe2V"}>
+            <span>
+              <PiDiscordLogoLight className="text-3xl"/>
+            </span>
+          </Link>
+
+          <Link href={"https://github.com/archangel-12"}>
           <span>
-            <PiGithubLogoLight className="text-2xl" />
+            <PiGithubLogoLight className="text-3xl" />
           </span>
+          </Link>
+
         </div>
       </div>
     </div>
